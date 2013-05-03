a2047e80a0e2014390d9c271e69896cb272a4802
robinharms <robin@betahaus.net>
2013-05-03 13:12:15 +0200

    Swedish translation + some minor template fixes

--- voteit/debate/__init__.py
+++ voteit/debate/__init__.py
@@ -10,6 +10,8 @@ def includeme(config):
     config.scan()
     from .models import SpeakerListHandler
     config.registry.registerAdapter(SpeakerListHandler)
+    
+    config.add_translation_dirs('%s:locale/' % PROJECTNAME)
 
     cache_ttl_seconds = int(config.registry.settings.get('cache_ttl_seconds', 7200))
     config.add_static_view('debate_static', '%s:static' % PROJECTNAME, cache_max_age = cache_ttl_seconds)
@@ -17,6 +19,5 @@ def includeme(config):
     _ = DebateTSF
     js_util = config.registry.getUtility(IJSUtil)
     js_util.add_translations(
-        sort_when_timer_active_error = _(u"Can't sort when timer is running"),
         nothing_to_start_error = _(u"Nothing to start"),
         speaker_already_in_list = _(u"Speaker already in this list"),)
