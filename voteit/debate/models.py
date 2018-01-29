587276ce80658f165e8061698590e71ff4643e5b
Robin Harms Oredsson <robin@betahaus.net>
2018-01-29 08:56:00 +0100

    Nonworking sketch

--- voteit/debate/models.py
+++ voteit/debate/models.py
@@ -1,86 +1,75 @@
+# -*- coding: utf-8 -*-
+from UserDict import IterableUserDict
 from datetime import timedelta
-from persistent import Persistent
 from random import shuffle
 
-from BTrees.IOBTree import IOBTree
 from BTrees.OOBTree import OOBTree
+from BTrees.IOBTree import IOBTree
+from persistent import Persistent
+
 from arche.interfaces import IObjectUpdatedEvent
 from arche.portlets import get_portlet_manager
 from persistent.list import PersistentList
 from pyramid.decorator import reify
-from pyramid.threadlocal import get_current_registry
-from pyramid.threadlocal import get_current_request
-from pyramid.traversal import find_interface
+from pyramid.interfaces import IRequest
+from six import string_types
 from voteit.core.models.interfaces import IAgendaItem
 from voteit.core.models.interfaces import IMeeting
-from voteit.irl.models.interfaces import IParticipantNumbers
 from zope.component import adapter
 from zope.interface import implementer
 
-from voteit.debate import _
 from voteit.debate.interfaces import ISpeakerList
-from voteit.debate.interfaces import ISpeakerListPlugin
 from voteit.debate.interfaces import ISpeakerLists
+from voteit.debate import _
 
 
 @implementer(ISpeakerLists)
-@adapter(IMeeting)
-class SpeakerLists(object):
+@adapter(IMeeting, IRequest)
+class SpeakerLists(IterableUserDict):
     """ See .interfaces.ISpeakerLists """
+    name = ""
+    title = _("Default list handler")
+    description = ""
 
-    def __init__(self, context):
+    def __init__(self, context, request):
         self.context = context
-
-    def __nonzero__(self):
-        return True #Otherwise this object will eval as false if no speaker lists are present
-
-    @reify
-    def _registry(self):
-        return get_current_registry()
-
-    @reify
-    def speaker_list_plugin(self):
-        return self.context.get_field_value('speaker_list_plugin', '')
+        self.request = request
 
     @property
-    def speaker_lists(self):
+    def data(self):
         try:
             return self.context.__speaker_lists__
         except AttributeError:
             self.context.__speaker_lists__ = OOBTree()
             return self.context.__speaker_lists__
 
-    @property
-    def active_list_name(self, default = None):
-        return getattr(self.context, '__active_speaker_list__', default)
+    @reify
+    def settings(self):
+#        schema = self.request.get_schema(context, context.type_name, 'edit', bind=None, event=True)
+        from voteit.debate.schemas import SpeakerListSettingsSchema
+        schema = SpeakerListSettingsSchema()
+        #FIXME: use get_schema on request instead
+        #Should map default values from schema
+        settings = dict(speaker_list_count = 9,
+                        safe_positions = 1)
+        settings.update(self.context.get_field_appstruct(schema))
+        return settings
 
-    @active_list_name.setter
-    def active_list_name(self, key):
-        if key is None or key in self.speaker_lists:
-            self.context.__active_speaker_list__ = key
-            return key
-        raise KeyError("No list named '%s'" % key)
+    # @property
+    # def active_list_name(self, default = None):
+    #     return getattr(self.context, '__active_speaker_list__', default)
+    #
+    # @active_list_name.setter
+    # def active_list_name(self, key):
+    #     if key is None or key in self.speaker_lists:
+    #         self.context.__active_speaker_list__ = key
+    #         return key
+    #     raise KeyError("No list named '%s'" % key)
 
-    def add(self, key, value, parent = None):
-        if not ISpeakerList.providedBy(value):
-            raise TypeError("Only objects implementing ISpeakerList allowed")
-        if parent is None:
-            request = get_current_request()
-            parent = request.context
-        assert IAgendaItem.providedBy(parent)
-        value.__parent__ = parent #To hack in traversal
-        self.speaker_lists[key] = value
-        return key
-
-    def get(self, key, default = None):
-        if key and key in self:
-            return self[key]
-        return default
-
-    def get_contexual_list_names(self, context):
+    def get_list_names(self, uid):
         results = []
-        for name in self.speaker_lists:
-            if context.uid in name:
+        for name in self:
+            if uid in name:
                 results.append(name)
         def _sorter(obj):
             try:
@@ -89,13 +78,14 @@ class SpeakerLists(object):
                 return 0 #b/c compat
         return sorted(results, key = _sorter)
 
-    def get_contextual_lists(self, context):
-        return [self.get(x) for x in self.get_contexual_list_names(context)]
+    def get_lists_in(self, uid):
+        return [self.get(x) for x in self.get_list_names(uid)]
 
-    def add_contextual_list(self, context):
-        existing = self.get_contexual_list_names(context)
+    def add_list_to(self, context):
+        assert IAgendaItem.providedBy(context)
+        existing = self.get_list_names(context.uid)
         if not existing:
-            name = "%s/1" % context.uid
+            key = "%s/1" % context.uid
             title = context.title
         else:
             last_list = existing[-1]
@@ -104,193 +94,82 @@ class SpeakerLists(object):
                 i = int(items[1]) + 1
             except IndexError: # pragma: no cover
                 i = 1 #b/c compat
-            name = "%s/%s" % (context.uid, i)
+            key = "%s/%s" % (context.uid, i)
             title = "%s - %s" % (context.title, i)
-        sl = SpeakerList(name, title = title)
-        return self.add(name, sl, parent = context)
+        sl = SpeakerList(key, title = title)
+        sl.__parent__ = context
+        self[key] = sl
+        return sl
 
-    def __getitem__(self, key): #dict api
-        sl = self.speaker_lists[key]
-        return self._registry.getAdapter(sl, ISpeakerListPlugin, name = self.speaker_list_plugin)
-
-    def __setitem__(self, key, value): #dict api
-        return self.add(key, value)
+    def __setitem__(self, key, sl): #dict api
+        if not ISpeakerList.providedBy(sl):
+            raise TypeError("Only objects implementing ISpeakerList allowed")
+        assert sl.__parent__ is not None
+        self.data[key] = sl
 
     def __delitem__(self, key): #dict api
         if self.active_list_name == key:
             self.active_list_name = None
-        del self.speaker_lists[key]
+        del self.data[key]
 
-    def __len__(self): #dict api
-        return len(self.speaker_lists)
+    def __nonzero__(self):
+        #Make sure the adapter registers as true even if it's empty
+        return True
 
-    def __contains__(self, key): #dict api
-        return key in self.speaker_lists
-
-
-_POSSIBLE_STATES = {u"open": _(u"Open"), u"closed": _(u"Closed")}
-
-
-@implementer(ISpeakerListPlugin)
-@adapter(ISpeakerList)
-class SpeakerListPlugin(object):
-    """ See .interfaces.ISpeakerListPlugin """
-    plugin_name = u''
-    plugin_title = _(u"Default list handler")
-    plugin_description = u""
-    __parent__ = None
-
-    def __init__(self, context):
-        self.context = context
-        self.__parent__ = context.__parent__
-
-    def __eq__(self, other):
-        return self.plugin_name == getattr(other, 'plugin_name', None) and self.name == getattr(other, 'name', None)
-
-    @property
-    def name(self):
-        return self.context.name
-
-    @property
-    def title(self):
-        return self.context.title
-
-    @title.setter
-    def title(self, value):
-        self.context.title = value
-
-    @property
-    def speakers(self):
-        return self.context.speakers
-
-    @property
-    def speaker_log(self):
-        return self.context.speaker_log
-
-    @property
-    def current(self):
-        return self.context.current
-
-    @current.setter
-    def current(self, value):
-        self.context.current = value
-
-    @property
-    def state(self):
-        return self.context.state
-
-    @state.setter
-    def state(self, value):
-        assert value in _POSSIBLE_STATES
-        self.context.state = value
-
-    @reify
-    def settings(self):
-        #FIXME: This should be refactored...
-        meeting = find_interface(self.context, IMeeting)
-        if meeting is None:
-            request = get_current_request()
-            meeting = request.meeting
-        from voteit.debate.schemas import SpeakerListSettingsSchema
-        schema = SpeakerListSettingsSchema()
-        #Should map default values from schema
-        settings = dict(speaker_list_count = 9,
-                        safe_positions = 1)
-        settings.update(meeting.get_field_appstruct(schema))
-        return settings
-
-    def add(self, pn, override = False):
+    def add_to_list(self, pn, sl, override = False):
         assert isinstance(pn, int)
-        if not override and self.state == u"closed":
+        if not override and sl.state == "closed":
             return
-        if pn in self.speakers:
+        if pn in sl:
             return
-        pos = self.get_position(pn)
-        self.speakers.insert(pos, pn)
+        pos = self.get_position(pn, sl)
+        sl.speakers.insert(pos, pn)
 
-    def get_position(self, pn):
-        safe_pos = self.settings.get('safe_positions')
-        compare_val = self.get_number_for(pn)
-        pos = len(self.speakers)
-        for speaker in reversed(self.speakers):
-            if pos == safe_pos:
-                break
-            if compare_val >= self.get_number_for(speaker):
-                break
-            pos -= 1
-        return pos
-
-    def get_stats(self, pn, format = True):
-        if pn not in self.speaker_log:
-            return (0, 0)
-        time = sum(self.speaker_log[pn])
-        if format:
-            time = unicode(timedelta(seconds = time))
-        return (len(self.speaker_log[pn]), time)
-
-    def shuffle(self):
+    def shuffle(self, sl):
         use_lists = self.settings.get('speaker_list_count')
         lists = {}
-        for speaker in self.speakers:
-            cmp_val = len(self.speaker_log.get(speaker, ())) + 1
+        for speaker in sl:
+            cmp_val = len(sl.speaker_log.get(speaker, ())) + 1
             if cmp_val > use_lists:
                 cmp_val = use_lists
             cur = lists.setdefault(cmp_val, [])
             cur.append(speaker)
-        del self.speakers[:]
+        del sl.speakers[:]
         for i in range(1, use_lists + 1):
             if i in lists:
                 shuffle(lists[i])
-                self.speakers.extend(lists[i])
+                sl.speakers.extend(lists[i])
 
-    def get_number_for(self, pn):
+    def get_position(self, pn, sl):
+        safe_pos = self.settings.get('safe_positions')
+        compare_val = self.get_list_number_for(pn, sl)
+        pos = len(self)
+        for speaker in reversed(sl):
+            if pos == safe_pos:
+                break
+            if compare_val >= self.get_list_number_for(speaker, sl):
+                break
+            pos -= 1
+        return pos
+
+    def get_list_number_for(self, pn, sl):
         assert isinstance(pn, int)
+        assert ISpeakerList.providedBy(sl)
         use_lists = self.settings.get('speaker_list_count')
-        cmp_val = len(self.speaker_log.get(pn, ())) + 1 #1 log entry means secondary speaker list - a val of 2
+        # 1 log entry means secondary speaker list - a val of 2
+        cmp_val = len(sl.speaker_log.get(pn, ())) + 1
         return cmp_val <= use_lists and cmp_val or use_lists
-
-    def speaker_active(self, pn):
-        assert isinstance(pn, int)
-        if pn in self.speakers:
-            self.current = pn
-            self.speakers.remove(pn)
-            return pn
-
-    def speaker_finished(self, pn, seconds):
-        assert isinstance(pn, int)
-        assert isinstance(seconds, int)
-        if self.current != pn:
-            return
-        if self.current not in self.speaker_log:
-            self.speaker_log[self.current] = PersistentList()
-        self.speaker_log[self.current].append(seconds)
-        self.current = None
-        return pn
-
-    def speaker_undo(self):
-        if self.current == None:
-            return
-        self.speakers.insert(0, self.current)
-        pn = self.current
-        self.current = None
-        return pn
-
-    def get_state_title(self):
-        return _POSSIBLE_STATES.get(self.state, u"")
-
-    def __repr__(self): # pragma : no cover
-        return "<%s> named '%s' adapting %s" % (self.__class__.__name__, self.name, self.context.__repr__())
 
 
 @implementer(ISpeakerList)
 class SpeakerList(Persistent):
-    name = u""
+    name = ""
     current = None
-    title = u""
-    state = u""
+    title = ""
+    state = ""
     __parent__ = None
 
-    def __init__(self, name, title = u"", state = u"open"):
+    def __init__(self, name, title = "", state = "open"):
         self.name = name
         self.speakers = PersistentList()
         self.speaker_log = IOBTree()
@@ -298,35 +177,77 @@ class SpeakerList(Persistent):
         self.title = title
         self.state = state
 
+    def start(self, pn):
+        assert isinstance(pn, int)
+        if pn in self:
+            self.current = pn
+            self.speakers.remove(pn)
+            return pn
+
+    def finish(self, pn, seconds):
+        if self.current != pn:
+            return
+        assert isinstance(pn, int)
+        assert isinstance(seconds, int)
+        if self.current not in self.speaker_log:
+            self.speaker_log[self.current] = PersistentList()
+        self.speaker_log[self.current].append(seconds)
+        self.current = None
+        return pn
+
+    def undo(self):
+        if self.current == None:
+            return
+        self.speakers.insert(0, self.current)
+        pn = self.current
+        self.current = None
+        return pn
+
     def __repr__(self): # pragma : no cover
         return "<%s> '%s' with %s speakers" % (self.__class__.__name__, self.name, len(self.speakers))
 
+    def __contains__(self, item):
+        return item in self.speakers
 
-def populate_from_proposals(sl, request = None):
-    if request is None:
-        request = get_current_request()
-    ai = find_interface(sl, IAgendaItem)
-    assert ai
-    meeting = find_interface(sl, IMeeting)
-    assert meeting
-    participant_numbers = request.registry.getAdapter(meeting, IParticipantNumbers)
-    handled_userids = set()
-    found = 0
-    for prop in ai.get_content(content_type = 'Proposal', states = ['published'], sort_on = 'created'):
-        userid = prop.creators[0]
-        if userid in handled_userids:
-            continue
-        handled_userids.add(userid)
-        pn = participant_numbers.userid_to_number.get(userid, None)
-        if not pn or pn in sl.speakers:
-            continue
-        sl.add(pn, override = True)
-        found += 1
-    return found
+    def __iter__(self):
+        return iter(self.speakers)
+
+    def __len__(self):
+        return len(self.speakers)
 
 
-def get_speaker_list_plugins(request):
-    return [(x.name, x.factory.plugin_title) for x in request.registry.registeredAdapters() if x.provided == ISpeakerListPlugin]
+def speaker_lists(request, meeting=None):
+    """ Will fetch currently set ISpeakerLists adapter.
+        Since this will be a cached property on the request object,
+        the meeting argument is only for testing.
+    """
+    if not meeting:
+        meeting = request.meeting
+    return request.registry.getMultiAdapter([meeting, request], ISpeakerLists,
+                                            name=meeting.speaker_list_plugin)
+
+
+# def populate_from_proposals(sl, request = None):
+#     if request is None:
+#         request = get_current_request()
+#     ai = find_interface(sl, IAgendaItem)
+#     assert ai
+#     meeting = find_interface(sl, IMeeting)
+#     assert meeting
+#     participant_numbers = request.registry.getAdapter(meeting, IParticipantNumbers)
+#     handled_userids = set()
+#     found = 0
+#     for prop in ai.get_content(content_type = 'Proposal', states = ['published'], sort_on = 'created'):
+#         userid = prop.creators[0]
+#         if userid in handled_userids:
+#             continue
+#         handled_userids.add(userid)
+#         pn = participant_numbers.userid_to_number.get(userid, None)
+#         if not pn or pn in sl.speakers:
+#             continue
+#         sl.add(pn, override = True)
+#         found += 1
+#     return found
 
 
 def insert_portlet_when_enabled(context, event):
@@ -354,12 +275,23 @@ def insert_portlet_when_enabled(context, event):
 
 
 def includeme(config):
-    config.registry.registerAdapter(SpeakerLists)
-    config.registry.registerAdapter(SpeakerListPlugin)
+    config.add_request_method(speaker_lists, reify=True)
+    # The default one won't have a name
+    config.registry.registerAdapter(SpeakerLists, name=SpeakerLists.name)
     config.add_subscriber(insert_portlet_when_enabled, [IMeeting, IObjectUpdatedEvent])
+
     from voteit.core.models.meeting import Meeting
+    #Bool property for enabled plugin
     def get_enable_voteit_debate(self):
         return self.get_field_value('enable_voteit_debate', None)
     def set_enable_voteit_debate(self, value):
         return self.set_field_value('enable_voteit_debate', bool(value))
     Meeting.enable_voteit_debate = property(get_enable_voteit_debate, set_enable_voteit_debate)
+
+    #Property for adapter type
+    def get_speaker_list_plugin(self):
+        return self.get_field_value('speaker_list_plugin', '')
+    def set_speaker_list_plugin(self, value):
+        assert isinstance(value, string_types)
+        self.set_field_value('speaker_list_plugin', value)
+    Meeting.speaker_list_plugin = property(get_speaker_list_plugin, set_speaker_list_plugin)
