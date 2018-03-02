96962143ff8148e2ffe67515f9ec411b56e3efc9
Robin Harms Oredsson <robin@betahaus.net>
2018-03-02 17:45:49 +0100

    better translations script

--- update_translations.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
- #You need lingua and gettext installed to run this
- 
- echo "Updating voteit.debate.pot"
- pot-create -d voteit.debate -o voteit/debate/locale/voteit.debate.pot .
- echo "Merging Swedish localisation"
- msgmerge --update voteit/debate/locale/sv/LC_MESSAGES/voteit.debate.po voteit/debate/locale/voteit.debate.pot
- echo "Updated locale files"
- 
