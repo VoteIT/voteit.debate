b4555c76b9bfd3efe1ded69743facd4fd739eea2
Robin Harms Oredsson <robin@betahaus.net>
2018-04-21 08:25:26 +0200

    Show minutes fix

--- voteit/debate/plugins/global_lists/__init__.py
+++ voteit/debate/plugins/global_lists/__init__.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from collections import Counter
 
-from voteit.debate.models import SpeakerLists
+import colander
+from arche.interfaces import ISchemaCreatedEvent
+from pyramid.decorator import reify
+
 from voteit.debate import _
+from voteit.debate.models import SpeakerLists
+from voteit.debate.schemas import SpeakerListSettingsSchema
 
 
 class GlobalLists(SpeakerLists):
@@ -11,7 +16,6 @@ class GlobalLists(SpeakerLists):
     title = _("Global timelog")
     description = _("Monitors and shows total entries")
     tpl_manage_speaker_item = 'voteit.debate:plugins/global_lists/templates/manage_speaker_item.pt'
-    #tpl_user = 'voteit.debate:templates/snippets/speaker_item_user.pt'
 
     def total_count(self, pns):
         # FIXME: Should be cached later on
@@ -22,6 +26,48 @@ class GlobalLists(SpeakerLists):
                     counter.update({k: len(v)})
         return counter
 
+    @reify
+    def time_restrictions(self):
+        return self.settings.get('global_time_restrictions', ())
+
+    def get_time_restr(self, num):
+        if self.time_restrictions:
+            try:
+                return self.time_restrictions[num]
+            except IndexError:
+                return self.time_restrictions[-1]
+
+
+def update_schema(schema, event):
+    request = event.request
+    if request.speaker_lists.name != 'global_lists':
+        return
+    schema.add(
+        colander.SchemaNode(
+            colander.Sequence(),
+            colander.SchemaNode(
+                colander.Int(),
+                title = _("minute restriction"),
+                name='foo',  # Not used
+            ),
+            title=_("Speaker time global entries"),
+            description=_(
+                "speaker_time_global_entries_description",
+                default="Add entries to show time restriction. "
+                        "No entries mean no restriction. "
+                        "If you add entries, they will be shown in sequence."
+                        "The last one will be used for anyone "
+                        "above the number of entries. "
+                        "So 2 rows with 2 and then 1 would mean 2 minutes "
+                        "for first entry, and 1 minute for all other entries."
+            ),
+            name='global_time_restrictions',
+            missing=(),
+            tab='advanced',
+        )
+    )
+
 
 def includeme(config):
     config.registry.registerAdapter(GlobalLists, name=GlobalLists.name)
+    config.add_subscriber(update_schema, [SpeakerListSettingsSchema, ISchemaCreatedEvent])
