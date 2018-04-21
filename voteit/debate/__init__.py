fc27b5c24cf666f92b223fa6cf55aae79576f8c0
Robin Harms Oredsson <robin@betahaus.net>
2018-04-21 16:18:05 +0200

    Not used anylonger

--- voteit/debate/__init__.py
+++ voteit/debate/__init__.py
@@ -1,5 +1,4 @@
 import logging
-from voteit.core.models.interfaces import IJSUtil
 
 from pyramid.i18n import TranslationStringFactory
 
@@ -18,11 +17,3 @@ def includeme(config):
     config.add_translation_dirs('%s:locale/' % PROJECTNAME)
     cache_max_age = int(config.registry.settings.get('arche.cache_max_age', 60*60*24))
     config.add_static_view('debate_static', '%s:static' % PROJECTNAME, cache_max_age = cache_max_age)
-    js_util = config.registry.queryUtility(IJSUtil)
-    if js_util:
-        #Since this is fine during tests, we're okay with skipping js translations
-        js_util.add_translations(
-            nothing_to_start_error = _(u"Nothing to start"),
-            speaker_already_in_list = _(u"Speaker already in this list"),)
-    else:
-        logger.warn(u"JSUtil not found during startup - ignoring js translations")
