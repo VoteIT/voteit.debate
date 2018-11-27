a474c9cecd3b78a4df2453dda348e30c857dacf0
Robin Harms Oredsson <robin@betahaus.net>
2018-11-27 13:47:15 +0100

    Skip migrations for new dbs

--- voteit/debate/evolve/__init__.py
+++ voteit/debate/evolve/__init__.py
@@ -8,7 +8,7 @@ VERSION = 1
 class DebateEvolver(BaseEvolver):
     name = 'voteit.debate'
     sw_version = VERSION
-    initial_db_version = 0
+    initial_db_version = VERSION
 
 
 def includeme(config):
