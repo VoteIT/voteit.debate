c72b98cffaa050d4540e24f11abdef98d69d0bdd
Robin Harms Oredsson <robin@betahaus.net>
2020-04-14 14:25:39 +0200

    Check portlet visibility

--- voteit/debate/portlet.py
+++ voteit/debate/portlet.py
@@ -13,19 +13,21 @@ class DebatePortlet(PortletType):
     title = _("Debate")
     tpl = "voteit.debate:templates/portlet.pt"
 
+    def visible(self, context, request, view, **kwargs):
+        return IAgendaItem.providedBy(context)
+
     def render(self, context, request, view, **kwargs):
-        if IAgendaItem.providedBy(context):
-            pns = IParticipantNumbers(request.meeting, None)
-            pn = None
-            if pns:
-                pn = pns.userid_to_number.get(request.authenticated_userid, None)
-            update_interv = request.speaker_lists.settings.get('user_update_interval', 5)
-            return render(self.tpl,
-                          {'portlet': self.portlet,
-                           'view': view,
-                           'user_update_interval': update_interv,
-                           'pn': pn},
-                          request=request)
+        pns = IParticipantNumbers(request.meeting, None)
+        pn = None
+        if pns:
+            pn = pns.userid_to_number.get(request.authenticated_userid, None)
+        update_interv = request.speaker_lists.settings.get('user_update_interval', 5)
+        return render(self.tpl,
+                      {'portlet': self.portlet,
+                       'view': view,
+                       'user_update_interval': update_interv,
+                       'pn': pn},
+                      request=request)
 
 
 def includeme(config):
